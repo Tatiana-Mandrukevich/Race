9055b7fab8b7b68755b5e678956c4e5daf6231e7
Tatiana Mandrukevich <tatyana.zharikova.jazzteam@gmail.com>
2026-07-18 22:59:31 +0300

    Added car Idle sound.

--- Assets/Scripts/StateMachine/IdleStateForCar.cs
+++ Assets/Scripts/StateMachine/IdleStateForCar.cs
@@ -6,11 +6,13 @@ public class IdleStateForCar : State
     private Transform _car;
     private Sequence _carSequence;
     private Vector3 _startScale;
+    private AudioManager _audioManager;
     
-    public IdleStateForCar(Transform car)
+    public IdleStateForCar(Transform car, AudioManager audioManager)
     {
         _car = car;
         _startScale = car.localScale;
+        _audioManager = audioManager;
     }
 
     public override void OnStateEnter()
@@ -18,11 +20,13 @@ public class IdleStateForCar : State
         _carSequence = DOTween.Sequence();
         _carSequence.Append(_car.DOShakeScale(0.2f, 0.01f));
         _carSequence.SetLoops(-1, LoopType.Yoyo);
+        _audioManager.PlayCarIdle();
     }
 
     public override void OnStateExit()
     {
         _carSequence.Kill();
         _car.transform.localScale = _startScale;
+        _audioManager.StopCarLoop();
     }
 }
